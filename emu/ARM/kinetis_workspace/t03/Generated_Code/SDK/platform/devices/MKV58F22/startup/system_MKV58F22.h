ee082611aa4489bb6bf1db933668eda5002fd4f7
Manabu Okada <nic@mak.name>
2016-08-14 10:20:09 +0900

    KV58プロジェクト追加

---
+++ emu/ARM/kinetis_workspace/t03/Generated_Code/SDK/platform/devices/MKV58F22/startup/system_MKV58F22.h
@@ -0,0 +1,180 @@
+/*
+** ###################################################################
+**     Processors:         MKV58F22
+**
+**
+**     Abstract:
+**         Provides a system configuration function and a global variable that
+**         contains the system frequency. It configures the device and initializes
+**         the oscillator (PLL) that is part of the microcontroller device.
+**
+**     Copyright : 1997 - 2015 Freescale Semiconductor, Inc. 
+**     All Rights Reserved.
+**     
+**     Redistribution and use in source and binary forms, with or without modification,
+**     are permitted provided that the following conditions are met:
+**     
+**     o Redistributions of source code must retain the above copyright notice, this list
+**       of conditions and the following disclaimer.
+**     
+**     o Redistributions in binary form must reproduce the above copyright notice, this
+**       list of conditions and the following disclaimer in the documentation and/or
+**       other materials provided with the distribution.
+**     
+**     o Neither the name of Freescale Semiconductor, Inc. nor the names of its
+**       contributors may be used to endorse or promote products derived from this
+**       software without specific prior written permission.
+**     
+**     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+**     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+**     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+**     DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+**     ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+**     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+**     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+**     ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+**     (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+**     SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+**     
+**     http: www.freescale.com
+**     mail: support@freescale.com
+**
+** ###################################################################
+*/
+
+/*!
+ * @file MKV58F22
+ * @brief Device specific configuration file for MKV58F22 (header file)
+ *
+ * Provides a system configuration function and a global variable that contains
+ * the system frequency. It configures the device and initializes the oscillator
+ * (PLL) that is part of the microcontroller device.
+ */
+
+#ifndef SYSTEM_MKV58F22_H_
+#define SYSTEM_MKV58F22_H_                     /**< Symbol preventing repeated inclusion */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#ifndef DISABLE_WDOG
+  #define DISABLE_WDOG                 1
+#endif
+
+/* Index of selected clock configuration */
+#ifndef CLOCK_SETUP
+  #define CLOCK_SETUP   0
+#endif
+
+/* MCG mode constants */
+
+#define MCG_MODE_FEI                   0U
+#define MCG_MODE_FBI                   1U
+#define MCG_MODE_BLPI                  2U
+#define MCG_MODE_FEE                   3U
+#define MCG_MODE_FBE                   4U
+#define MCG_MODE_BLPE                  5U
+#define MCG_MODE_PBE                   6U
+#define MCG_MODE_PEE                   7U
+
+/* Predefined clock setups
+   0 ... Default part configuration
+         Multipurpose Clock Generator (MCG) in FEI mode.
+         Reference clock source for MCG module: Slow internal reference clock
+         Core clock = 95.977472MHz
+         Bus clock  = 95.977472MHz
+*/
+
+/* Define clock source values */
+                
+#define CPU_XTAL_CLK_HZ                0UL                 /* System oscillator 0 is not enabled */
+#define CPU_INT_SLOW_CLK_HZ            32768UL             /* Value of the slow internal oscillator clock frequency in Hz  */
+#define CPU_INT_FAST_CLK_HZ            4000000UL           /* Value of the fast internal oscillator clock frequency in Hz  */
+
+/* Low power mode enable */
+
+/* SMC_PMPROT: AHSRUN=0,??=0,AVLP=0,??=0,??=0,??=0,AVLLS=0,??=0 */
+#define SYSTEM_SMC_PMPROT_VALUE         0x00U              /* No low power mode allowed */
+
+/* Internal reference clock trim */
+
+/* #undef SLOW_TRIM_ADDRESS */                             /* Slow oscillator not trimmed. Commented out for MISRA compliance. */
+/* #undef SLOW_FINE_TRIM_ADDRESS */                        /* Slow oscillator not trimmed. Commented out for MISRA compliance. */
+/* #undef FAST_TRIM_ADDRESS */                             /* Fast oscillator not trimmed. Commented out for MISRA compliance. */
+/* #undef FAST_FINE_TRIM_ADDRESS */                        /* Fast oscillator not trimmed. Commented out for MISRA compliance. */
+
+/* System clock */
+
+#ifdef CLOCK_SETUP 
+#if (CLOCK_SETUP == 0)
+  #define DEFAULT_SYSTEM_CLOCK          95977472UL         /* Default System clock value */
+  #define MCG_MODE                      MCG_MODE_FEI       /* Clock generator mode */
+  /* MCG_C1: CLKS=0,FRDIV=0,IREFS=1,IRCLKEN=1,IREFSTEN=0 */
+  #define SYSTEM_MCG_C1_VALUE           0x06U              /* MCG_C1 */
+  /* MCG_C2: LOCRE0=0,FCFTRIM=0,RANGE=0,HGO=0,EREFS=0,LP=0,IRCS=0 */
+  #define SYSTEM_MCG_C2_VALUE           0x00U              /* MCG_C2 */
+  /* MCG_C4: DMX32=1,DRST_DRS=3,FCTRIM=0,SCFTRIM=0 */
+  #define SYSTEM_MCG_C4_VALUE           0xE0U              /* MCG_C4 */
+  /* MCG_C5: PLLCLKEN=0,PLLSTEN=0,PRDIV=0 */
+  #define SYSTEM_MCG_C5_VALUE           0x00U              /* MCG_C5 */
+  /* MCG_C6: LOLIE0=0,PLLS=0,CME0=0,VDIV=0 */
+  #define SYSTEM_MCG_C6_VALUE           0x00U              /* MCG_C6 */
+  /* MCG_SC: ATME=0,ATMS=0,ATMF=0,FLTPRSRV=0,FCRDIV=0,LOCS0=0 */
+  #define SYSTEM_MCG_SC_VALUE           0x00U              /* MCG_SC */
+  /* OSC0_CR: ERCLKEN=1,EREFSTEN=0,SC2P=0,SC4P=0,SC8P=0,SC16P=0 */
+  #define SYSTEM_OSC0_CR_VALUE          0x80U              /* OSC0_CR */
+  /* OSC0_OSC_DIV: ERPS=0 */
+  #define SYSTEM_OSC0_OSC_DIV_VALUE     0x00U              /* OSC0_OSC_DIV */
+  /* SMC_PMCTRL: RUNM=0 */
+  #define SYSTEM_SMC_PMCTRL_VALUE       0x00               /* SMC_PMCTRL */
+  /* SIM_SOPT1: OSC32KSEL=0 */
+  #define SYSTEM_SIM_SOPT1_VALUE        0x00UL             /* SIM_SOPT1 */
+  /* SIM_CLKDIV1: OUTDIV1=0,OUTDIV2=0,OUTDIV3=3,OUTDIV4=3 */
+  #define SYSTEM_SIM_CLKDIV1_VALUE      0x00330000UL       /* SIM_CLKDIV1 */
+#endif
+#endif  /* CLOCK_SETUP */   
+
+
+/**
+ * @brief System clock frequency (core clock)
+ *
+ * The system clock frequency supplied to the SysTick timer and the processor
+ * core clock. This variable can be used by the user application to setup the
+ * SysTick timer or configure other parameters. It may also be used by debugger to
+ * query the frequency of the debug timer or configure the trace clock speed
+ * SystemCoreClock is initialized with a correct predefined value.
+ */
+extern uint32_t SystemCoreClock;
+
+/**
+ * @brief Setup the microcontroller system.
+ *
+ * Typically this function configures the oscillator (PLL) that is part of the
+ * microcontroller device. For systems with variable clock speed it also updates
+ * the variable SystemCoreClock. SystemInit is called from startup_device file.
+ */
+void SystemInit (void);
+
+/**
+ * @brief Updates the SystemCoreClock variable.
+ *
+ * It must be called whenever the core clock is changed during program
+ * execution. SystemCoreClockUpdate() evaluates the clock register settings and calculates
+ * the current core clock.
+ */
+void SystemCoreClockUpdate (void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* #if !defined(SYSTEM_MKV58F22_H_) */
+
+
+
+
+
+
